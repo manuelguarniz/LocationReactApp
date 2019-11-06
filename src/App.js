37d97115896f9d5b001478a02c5092fa0e0b6a09
Manuel Guarniz <cruzemg95@gmail.com>
2019-11-05 19:45:04 -0500

    add flex column

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import LocationList from './components/WeatherLocation/LocationList';
+import { Grid, Col, Row } from 'react-flexbox-grid';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
 const cities = [
   'Washington,us',
@@ -14,10 +16,25 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div className="App">
-        <LocationList cities={cities}
-          onSelectLocation={this.handleSelectionLocation}/>
-      </div>
+      <MuiThemeProvider>
+        <Grid fluid>
+          <Row>
+            <Col xs={12} sm={6} md={4}>
+              <div className='red'></div>
+            </Col>
+            <Col xs={12} sm={6} md={4}>
+              <div className='green'></div>
+            </Col>
+            <Col xs={12} sm={6} md={4}>
+              <div className='blue'></div>
+            </Col>
+          </Row>
+        </Grid>
+      </MuiThemeProvider>
+      // <div className="App">
+      //   <LocationList cities={cities}
+      //     onSelectLocation={this.handleSelectionLocation}/>
+      // </div>
     );
   }
 }
